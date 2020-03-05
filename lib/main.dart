68009bb0756e239cac64a3524216b119c0705c70
Nadeem Siddique <hellomr82k@gmail.com>
2020-03-05 23:11:26 +0530

    Update .gitignore, .project, and 8 more files... Added support for uploading screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'dart:convert';
+
 import 'package:after_layout/after_layout.dart';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:ural/auth_bloc.dart';
+import 'package:ural/urls.dart';
 import 'package:ural/user_dialog.dart';
 import 'package:ural/utils/async.dart';
 import 'textview.dart';
 import 'auth_dialog.dart';
 import 'dart:io';
-
-import 'package:ural/utils/bloc_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:image/image.dart' as img;
+import 'package:path_provider/path_provider.dart';
 
 void main() => runApp(App());
 
@@ -34,6 +38,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> with AfterLayoutMixin {
   File _image;
   final recognizer = FirebaseVision.instance.textRecognizer();
+  VisionText _visionText;
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
 
@@ -64,6 +69,50 @@ class _HomeState extends State<Home> with AfterLayoutMixin {
     // }
   }
 
+  Future<VisionText> getTextFromImage() async {
+    final fbImage = FirebaseVisionImage.fromFile(_image);
+    final visionText = await recognizer.processImage(fbImage);
+    return visionText;
+  }
+
+  Future<void> handleUpload() async {
+    if (_image != null) {
+      img.Image image = img.decodeImage(_image.readAsBytesSync());
+      img.Image thumbnail = img.copyResize(image, width: 120);
+      Directory tempDir = await getTemporaryDirectory();
+      String encoded;
+      var path = _image.path.split("/").last;
+      final filename = path.split(".")[0] + ".jpg";
+      File(tempDir.path + '/' + filename)
+        ..writeAsBytes(img.encodeJpg(thumbnail)).then((file) async {
+          encoded = base64.encode(await file.readAsBytes());
+          // encoded = file.readAsBytesSync().toString();
+        });
+      String url = ApiUrls.root + ApiUrls.images;
+      String text;
+      await getTextFromImage().then((obj) => text = obj.text);
+      String payload = json.encode({
+        "filename": filename,
+        "thumbnail": encoded,
+        "image_path": _image.path,
+        "text": text,
+        "short_text": "",
+      });
+      try {
+        final response = await http.post(url,
+            body: payload,
+            headers: ApiUrls.authenticatedHeader(Auth().user.token));
+        if (response.statusCode == 201) {
+          print("Image uploaded successfully");
+        } else {
+          print(response.body);
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -80,6 +129,14 @@ class _HomeState extends State<Home> with AfterLayoutMixin {
       );
   }
 
+  // Center(
+  //       child: Container(
+  //         height: MediaQuery.of(context).size.height * 0.8,
+  //         width: MediaQuery.of(context).size.width * 0.8,
+  //         child: _image == null ? Text("No image") : Image.file(_image),
+  //       ),
+  //     )
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,37 +150,63 @@ class _HomeState extends State<Home> with AfterLayoutMixin {
               })
         ],
       ),
-      body: Center(
-        child: Container(
-          height: MediaQuery.of(context).size.height * 0.8,
-          width: MediaQuery.of(context).size.width * 0.8,
-          child: _image == null ? Text("No image") : Image.file(_image),
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          FloatingActionButton(
-            heroTag: null,
-            onPressed: () async {
-              final blocks = await recognizeImage();
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      fullscreenDialog: true,
-                      builder: (context) => TextView(
-                            textBlocks: blocks,
-                          )));
-            },
-            child: Icon(Icons.text_fields),
+          Container(
+            padding: EdgeInsets.all(8),
+            height: 110,
+            width: MediaQuery.of(context).size.width,
+            child: Center(
+              child: Card(
+                child: ListTile(
+                  title: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Type what you're looking for here"),
+                  ),
+                  trailing:
+                      IconButton(icon: Icon(Icons.search), onPressed: null),
+                ),
+              ),
+            ),
           ),
-          SizedBox(
-            height: 40,
-          ),
-          FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: getImage,
-          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Wrap(
+              alignment: WrapAlignment.spaceEvenly,
+              children: <Widget>[
+                FloatingActionButton(
+                  elevation: 0,
+                  heroTag: null,
+                  onPressed: () async {
+                    handleUpload();
+                  },
+                  child: Icon(Icons.file_upload),
+                ),
+                FloatingActionButton(
+                  elevation: 0,
+                  heroTag: null,
+                  onPressed: () async {
+                    final blocks = await recognizeImage();
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            fullscreenDialog: true,
+                            builder: (context) => TextView(
+                                  textBlocks: blocks,
+                                )));
+                  },
+                  child: Icon(Icons.text_fields),
+                ),
+                FloatingActionButton(
+                  elevation: 0,
+                  child: Icon(Icons.add),
+                  onPressed: getImage,
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
--- lib/urls.dart
+++ lib/urls.dart
@@ -1,8 +1,11 @@
 class ApiUrls {
-  // static String root = "http://192.168.0.107:8000/";
-  static String root = "http://192.168.43.90:8000/";
+  static String root = "http://192.168.0.107:8000/";
+  // static String root = "http://192.168.43.90:8000/";
   static String auth = "auth/";
   static String user = "user/";
+  static String images = "images/";
 
   static Map<String, String> headers = {"Content-Type": "application/json"};
+  static Map<String, String> authenticatedHeader(String token) =>
+      {"Authorization": "Token $token", "Content-type": "application/json"};
 }
