b485550f17d9cc3fa8517c46908e2293f13fbab5
purplecandy <hellomr82k@gmaill.com>
2020-04-08 23:09:25 +0530

    Update screen_model.dart

--- flutter/lib/models/screen_model.dart
+++ flutter/lib/models/screen_model.dart
@@ -1,11 +1,12 @@
 class ScreenshotModel {
-  final int hash;
+  final int hash, docId;
   final String imagePath, text;
 
-  ScreenshotModel(this.hash, this.imagePath, this.text);
+  ScreenshotModel(this.hash, this.imagePath, this.text, [this.docId]);
 
   factory ScreenshotModel.fromMap(Map<String, dynamic> map) {
-    return ScreenshotModel(map["hash"], map["imagePath"], map["text"]);
+    return ScreenshotModel(
+        map["hash"], map["imagePath"], map["text"], map["docid"]);
   }
 
   Map<String, dynamic> toMap() {
